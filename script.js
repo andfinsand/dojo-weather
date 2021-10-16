888be270dd9c9ddc137846bdc3f3ab1a78f39e62
Andrew Finsand <andfinsand@gmail.com>
2021-10-16 15:32:53 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function alertCity(element) {
+    alert("Loading weather report...")
+}
+
+var cookies = document.querySelector("#footer")
+function acceptButton() {
+    cookies.remove()
+}
+
+var celsius = 0
+var fahrenheit = document.querySelector(".fahrenheit").value
